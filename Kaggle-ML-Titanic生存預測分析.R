dbceb22487582d055832e74e3d5782a1d3cc1b26
Asta Lee <astalee12@gmail.com>
2019-02-18 01:33:04 +0800

    Update Kaggle-ML-Titanic生存預測分析.R

--- Kaggle-ML-Titanic生存預測分析.R
+++ Kaggle-ML-Titanic生存預測分析.R
@@ -7,6 +7,7 @@ colSums(is.na(full))
 #處理NA值的時候到了
 Train$Age[is.na(Train$Age)] = mean(Train$Age, na.rm = TRUE)
 Test$Age[is.na(Test$Age)] = mean(Test$Age, na.rm = TRUE)
+full$Embarked[full$Embarked==""]="C"
 
 #train跟test的欄位不同，無法用rbind做合併，則使用bind_rows做合併
 install.packages("dplyr")
@@ -35,14 +36,26 @@ devtools::install_github("tidyverse/ggplot2")
 install.packages("ggplot2")
 library(ggplot2)
 
-#看看性別跟生存與否之間有沒有關係，看來男人死光光
+#看看性別跟生存與否之間有沒有關係，看來快男人死光光
 LT=dim(Train)[1]
 ggplot(data = full[1:LT,],aes(x=Sex,fill=Survived))+geom_bar()
 
 #看看Embarked跟生存與否之間有沒有關係
 ggplot(data = full[1:LT,],aes(x=Embarked,fill=Survived))+geom_bar(position="fill")+ylab("Frequency")
 
-#看看Pclss跟生存與否之間有沒有關係
+#看看Pclss跟生存與否之間有沒有關係，等級越高生存率越高
 ggplot(data = full[1:LT,],aes(x=Pclass,fill=Survived))+geom_bar(position="fill")+ylab("Frequency")
+
+#看看Pclss跟Embarked與生存與否之間有沒有關係
+ggplot(data = full[1:LT,],aes(x=Embarked,fill=Survived))+geom_bar(position="fill")+facet_wrap(~Pclass)
+
+#看看年紀跟生存會有什麼差別
+full$Age[is.na(full$Age)] <- mean(full$Age,na.rm=T)
+sum(is.na(full$Age))
+ggplot(full[1:LT,],aes(x=Age,fill=Survived))+geom_histogram(binwidth =3)
+
+#看看Fare跟生存會有什麼差別
+full$Fare[is.na(full$Fare)] <- mean(full$Fare,na.rm=T)
+ggplot(data = full[1:LT,],aes(x=Fare,fill=Survived))+geom_histogram(binwidth =20, position="fill")
 
 
